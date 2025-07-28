b15886970403e4f45895831422ac4137a2015236
Omer Aswad <mkf.dev.umar@gmail.com>
2025-07-28 14:51:37 +0500

    product route fixed

--- app/custom-packaging/[productSlug]/page.js
+++ app/custom-packaging/[productSlug]/page.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { connectDB } from "@/lib/mongoose"
 import Product from "@/app/models/Product"
 import { ProductHero, ContactForm, LongDescription, SpecificationTabs, ProductSpecifications, FaqSection, Testimonials, PackagingFeatures } from '@/app/components'
-// import { longDescription } from '@/app/constant'
 
 const page =  async ({params}) => {
   const { productSlug } = params;
