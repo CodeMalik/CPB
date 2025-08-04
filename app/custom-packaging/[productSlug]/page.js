04a5cf127e319b0774311f361a642f2d44178fa6
Omer Aswad <mkf.dev.umar@gmail.com>
2025-08-04 14:25:47 +0500

    product page error fixed

--- app/custom-packaging/[productSlug]/page.js
+++ app/custom-packaging/[productSlug]/page.js
@@ -4,10 +4,14 @@ import Product from "@/app/models/Product"
 import { ProductHero, LongDescription, SpecificationTabs, ProductSpecifications, FaqSection, Testimonials, PackagingFeatures } from '@/app/components'
 
 const page =  async ({params}) => {
+  await connectDB()
   const { productSlug } = params;
 
   const product = await Product.findOne({slug: productSlug})
-  await connectDB()
+
+  if (!product) {
+    return <p className='min-h-screen w-full flex items-center justify-center text-4xl text-red-themed'>This is the problem while rendring this page</p>
+  }
   return (
     <div className='mt-4'>
     <ProductHero heading={product.heading} shortDesc={product.shortDescription} tagline={product.tagline} images={product.images} category={product.categorySlug} name={product.name} />
