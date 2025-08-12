54b440c05bb24abe8e92ae801ef7375e45cc5b67
Omer Aswad <mkf.dev.umar@gmail.com>
2025-08-12 16:53:57 +0500

    schema fixed

--- app/models/Product.js
+++ app/models/Product.js
@@ -1,23 +1,18 @@
 import mongoose from "mongoose";
 
-const descriptionSchema = new mongoose.Schema({
-  title: String,
-  description: mongoose.Schema.Types.Mixed,
-});
-
 const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
   heading: { type: String, required: true },
   slug: {type: String, required: true},
-  categorySlug: {type: mongoose.Schema.Types.Mixed, required: true},
-  image: { type: String, required: true },
+  categorySlug: {type: String, required: true},
+  image: String,
   images: { type: [String], required: false },
   tagline: String,
   shortDescription: mongoose.Schema.Types.Mixed,
 
   categoryIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Category" }],
 
-  longDescription: [descriptionSchema],
+  longDescription: String,
   meta: {
     title: String,
     description: String,
