382847971e43530bcd4182b22b9ff3af454a4a91
Omer Aswad <mkf.dev.umar@gmail.com>
2025-09-09 13:04:11 +0500

    script tag added

--- app/components/AboutUs.jsx
+++ app/components/AboutUs.jsx
@@ -18,9 +18,8 @@ const AboutUs = () => {
               {aboutUsSection.aboutBenefits.map((benefit, index) => (
                 <div
                   key={index}
-                  className={`${
-                    index === 0 ? "md:col-span-2" : "md:col-span-1"
-                  }`}
+                  className={`${index === 0 ? "md:col-span-2" : "md:col-span-1"
+                    }`}
                 >
                   <h4 className="text-red-themed mb-3 text-lg font-bold">
                     {benefit.title && benefit.title}
@@ -31,32 +30,32 @@ const AboutUs = () => {
             </div>
             <a href="#quote" className="cursor-pointer">
               <button
-              className="
+                className="
   group inline-flex items-center gap-2 cursor-pointer px-6 py-3 my-6 text-lg font-semibold rounded-full 
   text-white bg-red-themed border border-red-themed 
   hover:bg-transparent hover:text-red-themed hover:border-red-themed
   transition-all duration-300 ease-in-out
   shadow-md hover:shadow-lg
 "
-            >
-              <span className="relative">
-                Request Custom Box
-                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-red-themed transition-all group-hover:w-full"></span>
-              </span>
-              <svg
-                className="w-5 h-5 transform transition-transform duration-300 group-hover:translate-x-1 group-hover:rotate-12"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                viewBox="0 0 24 24"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17 8l4 4m0 0l-4 4m4-4H3"
-                />
-              </svg>
-            </button>
+                <span className="relative">
+                  Request Custom Box
+                  <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-red-themed transition-all group-hover:w-full"></span>
+                </span>
+                <svg
+                  className="w-5 h-5 transform transition-transform duration-300 group-hover:translate-x-1 group-hover:rotate-12"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                  />
+                </svg>
+              </button>
             </a>
           </div>
           <div className="right flex-1">
