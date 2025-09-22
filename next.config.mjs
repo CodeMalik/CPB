ff5fd1b05b9aedbe06140e5f6724202318fe0ee4
Omer Aswad <mkf.dev.umar@gmail.com>
2025-09-22 15:24:12 +0500

    meta's fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
-    images: {
+  images: {
     domains: ["custompackboxes.com", "res.cloudinary.com", "images.unsplash.com", "cdn.pixabay.com", "timpackaging.com"],
   },
+  htmlLimitedBots: '.*',
 };
 
 export default nextConfig;
