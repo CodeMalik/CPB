8032b0989c55e445f73194fa05dd02ac67936d0b
mkfdevali-maker <mkf.dev.ali@gmail.com>
2025-11-18 19:17:55 +0500

    kilopol

--- app/components/CategoryHero.jsx
+++ app/components/CategoryHero.jsx
@@ -4,22 +4,22 @@ import Link from "next/link";
 
 const CategoryHero = ({ imageSrc, name }) => {
   return (
-    <section className="relative bg-white w-full h-[250px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[550px] overflow-hidden flex items-center justify-center">
+    <section className="relative bg-white w-full aspect-[21/9] min-h-[200px] max-h-[550px] overflow-hidden flex items-center justify-center border-l-3 border-r-3 border-white">
       {imageSrc ? (
-        <Image
-          src={imageSrc.url || imageSrc}
-          alt={name || "Category Image"}
-          fill
-          priority
-          className="object-cover lg:object-[center_top] w-full h-full max-sm:border-l-8 max-sm:border-r-8 max-sm:border-white"
-          sizes="100vw"
-          quality={90}
-        />
+       <Image
+  src={imageSrc.url || imageSrc}
+  alt={name || "Category Image"}
+  fill
+  priority
+  className="object-cover w-full h-full"
+  sizes="(max-width: 640px) 100vw, (max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
+  quality={85}
+/>
       ) : null}
 
       <div className="absolute inset-0 flex flex-col items-center justify-center text-center z-10">
-        <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl">{name}</h1>
-        <p className="mt-6 text-sm md:text-base lg:text-lg">
+        <h1 className="text-3xl md:text-4xl">{name}</h1>
+        <p className="mt-6 text-sm">
           <Link href={"/"}><span className="">Home</span></Link> <span className="mx-1">|</span>{" "}
           <span className="text-blue-400 underline">{name}</span>
         </p>
