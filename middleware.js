d7d87fbf39c9e64ebfabadc176fd0061171a46ca
mkfdevali-maker <mkf.dev.ali@gmail.com>
2026-05-31 14:40:24 +0500

    pushing middleware

--- middleware.js
+++ middleware.js
@@ -3,31 +3,32 @@ import { NextResponse } from 'next/server'
 // Only allow these countries - block everything else
 // Added 'PK' for Pakistan
 const ALLOWED_COUNTRIES = [
-  'AF', 'AX', 'AL', 'DZ', 'AS', 'AD', 'AO', 'AI', 'AQ', 'AG', 'AR', 'AM', 'AW', 'AU', 'AT', 'AZ',
-  'BS', 'BH', 'BD', 'BB', 'BY', 'BE', 'BZ', 'BJ', 'BM', 'BT', 'BO', 'BQ', 'BA', 'BW', 'BV', 'BR', 'IO', 'BN', 'BG', 'BF', 'BI',
-  'CV', 'KH', 'CM', 'CA', 'KY', 'CF', 'TD', 'CL', 'CN', 'CX', 'CC', 'CO', 'KM', 'CG', 'CD', 'CK', 'CR', 'CI', 'HR', 'CU', 'CW', 'CY', 'CZ',
-  'DK', 'DJ', 'DM', 'DO',
-  'EC', 'EG', 'SV', 'GQ', 'ER', 'EE', 'SZ', 'ET',
-  'FK', 'FO', 'FJ', 'FI', 'FR', 'GF', 'PF', 'TF',
-  'GA', 'GM', 'GE', 'DE', 'GH', 'GI', 'GR', 'GL', 'GD', 'GP', 'GU', 'GT', 'GG', 'GN', 'GW', 'GY',
-  'HT', 'HM', 'VA', 'HN', 'HK', 'HU',
-  'IS', 'IN', 'ID', 'IR', 'IQ', 'IE', 'IM', 'IL', 'IT',
-  'JM', 'JP', 'JE', 'JO',
-  'KZ', 'KE', 'KI', 'KP', 'KR', 'KW', 'KG',
-  'LA', 'LV', 'LB', 'LS', 'LR', 'LY', 'LI', 'LT', 'LU',
-  'MO', 'MG', 'MW', 'MY', 'MV', 'ML', 'MT', 'MH', 'MQ', 'MR', 'MU', 'YT', 'MX', 'FM', 'MD', 'MC', 'MN', 'ME', 'MS', 'MA', 'MZ', 'MM',
-  'NA', 'NR', 'NP', 'NL', 'NC', 'NZ', 'NI', 'NE', 'NG', 'NU', 'NF', 'MK', 'MP', 'NO',
-  'OM',
-  'PK', 'PW', 'PS', 'PA', 'PG', 'PY', 'PE', 'PH', 'PN', 'PL', 'PT', 'PR',
-  'QA',
-  'RE', 'RO', 'RU', 'RW',
-  'BL', 'SH', 'KN', 'LC', 'MF', 'PM', 'VC', 'WS', 'SM', 'ST', 'SA', 'SN', 'RS', 'SC', 'SL', 'SG', 'SX', 'SK', 'SI', 'SB', 'SO', 'ZA', 'GS', 'SS', 'ES', 'LK', 'SD', 'SR', 'SJ', 'SE', 'CH', 'SY',
-  'TW', 'TJ', 'TZ', 'TH', 'TL', 'TG', 'TK', 'TO', 'TT', 'TN', 'TR', 'TM', 'TC', 'TV',
-  'UG', 'UA', 'AE', 'GB', 'US', 'UM', 'UY', 'UZ',
-  'VU', 'VE', 'VN', 'VG', 'VI',
-  'WF', 'EH',
-  'YE',
-  'ZM', 'ZW'
+  // United States and its territories/islands
+  'US',  // United States
+  'AS',  // American Samoa
+  'GU',  // Guam
+  'MP',  // Northern Mariana Islands
+  'PR',  // Puerto Rico
+  'UM',  // U.S. Minor Outlying Islands
+  'VI',  // U.S. Virgin Islands
+  
+  // United Kingdom and its territories/islands
+  'GB',  // United Kingdom
+  'AI',  // Anguilla
+  'BM',  // Bermuda
+  'VG',  // British Virgin Islands
+  'KY',  // Cayman Islands
+  'FK',  // Falkland Islands
+  'GI',  // Gibraltar
+  'GG',  // Guernsey
+  'JE',  // Jersey
+  'IM',  // Isle of Man
+  'MS',  // Montserrat
+  'PN',  // Pitcairn Islands
+  'SH',  // Saint Helena
+  'GS',  // South Georgia & South Sandwich Islands
+  'TC',  // Turks and Caicos Islands
+  'IO'   // British Indian Ocean Territory
 ];
 // Optional: Add known VPN/Proxy IP ranges or specific IPs you want to block
 const BLOCKED_IPS = [
